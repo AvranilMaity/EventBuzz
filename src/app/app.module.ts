6d03c69ec2e98c02a894b26d3f4442ca008de867
AvranilMaity <avranilmaity97@gmail.com>
2020-05-22 00:20:45 +0530

    Remaster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { UserNavigationComponent } from './components/navigation/user-navigation/user-navigation.component';
-import { OrganizerNavigationComponent } from './components/navigation/organizer-navigation/organizer-navigation.component';
-import { CommonNavigationComponent } from './components/navigation/common-navigation/common-navigation.component';
 import { SignInComponent } from './components/common/sign-in/sign-in.component';
 import { SignUpComponent } from './components/common/sign-up/sign-up.component';
 import { EventCardComponent } from './components/common/event-card/event-card.component';
@@ -13,10 +10,6 @@ import { EventCollectionComponent } from './components/common/event-collection/e
 import { EventDetailsComponent } from './components/common/event-details/event-details.component';
 import { EventRegisterComponent } from './components/user/event-register/event-register.component';
 import { EventAddEditComponent } from './components/organizer/event-add-edit/event-add-edit.component';
-import { UserDetailsFormComponent } from './components/user/user-details-form/user-details-form.component';
-import { OrganizerDetailsFormComponent } from './components/organizer/organizer-details-form/organizer-details-form.component';
-import { UserDashboardComponent } from './components/user/user-dashboard/user-dashboard.component';
-import { OrganizerDashboardComponent } from './components/organizer/organizer-dashboard/organizer-dashboard.component';
 import { RegisterEventConfirmationComponent } from './components/user/register-event-confirmation/register-event-confirmation.component';
 import { AddEventConfirmationComponent } from './components/organizer/add-event-confirmation/add-event-confirmation.component';
 
@@ -24,9 +17,6 @@ import { AddEventConfirmationComponent } from './components/organizer/add-event-
   imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
-    UserNavigationComponent,
-    OrganizerNavigationComponent,
-    CommonNavigationComponent,
     SignInComponent,
     SignUpComponent,
     EventCardComponent,
@@ -34,10 +24,6 @@ import { AddEventConfirmationComponent } from './components/organizer/add-event-
     EventDetailsComponent,
     EventRegisterComponent,
     EventAddEditComponent,
-    UserDetailsFormComponent,
-    OrganizerDetailsFormComponent,
-    UserDashboardComponent,
-    OrganizerDashboardComponent,
     RegisterEventConfirmationComponent,
     AddEventConfirmationComponent,
   ],
--- src/app/components/common/sign-up/sign-up.component.ts
+++ src/app/components/common/sign-up/sign-up.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  proceed: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onProceed() {
+    console.log(this.proceed);
+    this.proceed = !this.proceed;
+    console.log(this.proceed);
   }
-
 }
