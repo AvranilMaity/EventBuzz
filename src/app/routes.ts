1dba6c8944af1a4361ffffa1a76570f681e6d154
Kingshuk <saha.kingshuk@yahoo.com>
2020-05-22 23:49:16 +0530

    Minor fix

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,11 +2,11 @@ import {Routes} from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { SignInComponent } from './components/common/sign-in/sign-in.component';
 import { SignUpComponent } from './components/common/sign-up/sign-up.component';
-import { EventDetailsComponent } from './components/user/event-details/event-details.component';
 import { EventDetailsOrgComponent } from './components/organizer/event-details-org/event-details-org.component';
 import { EventRegisterComponent } from './components/user/event-register/event-register.component';
 import { EventAddEditComponent } from './components/organizer/event-add-edit/event-add-edit.component';
 import { DashboardComponent } from './components/common/dashboard/dashboard.component';
+import { EventDetailsComponent } from './components/user/event-details/event-details.component';
 
 export const appRoutes: Routes = [
     {path: 'landing', component: LandingComponent},
