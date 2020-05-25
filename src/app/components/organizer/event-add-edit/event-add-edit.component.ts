9116bc9166c5c18ae2886f673030a146d00c48e8
AvranilMaity <avranilmaity97@gmail.com>
2020-05-25 23:18:47 +0530

    add event almost complete

--- src/app/components/organizer/event-add-edit/event-add-edit.component.ts
+++ src/app/components/organizer/event-add-edit/event-add-edit.component.ts
@@ -9,11 +9,37 @@ export class EventAddEditComponent implements OnInit {
   proceed: number;
   files: File[] = [];
   organizers: string[] = ['Self', 'Corporate'];
+  invitedUsers: string[] = [
+    'avraneel.babai@gmail.com',
+    'avranilmaity97@gmail.com',
+    'james@abc.com',
+    'peter@oiu.in',
+    'avranil.maity@icici.com',
+    'maityav@campbells.com',
+    'avranil@yashwin.com',
+    'kingshuk.saha@gmail.com',
+    'kingshuk@gmail.com',
+    'saha@abc.com',
+    'metheking@oiu.in',
+    'king.saha@icici.com',
+    'sahaki@campbells.com',
+    'kingshuk@yashwin.com',
+    'kingshuk@gmail.com',
+    'saha@abc.com',
+    'metheking@oiu.in',
+    'king.saha@icici.com',
+    'sahaki@campbells.com',
+    'kingshuk@yashwin.com',
+  ];
 
   constructor() {}
 
   ngOnInit() {
     this.proceed = 0;
+  }
+
+  onAddUser(newInvite: string) {
+    this.invitedUsers.push(newInvite);
   }
 
   onSelect(event) {
