5ca4600025580f59811493db08f3faf4d06a6a72
AvranilMaity <avranilmaity97@gmail.com>
2020-06-02 02:54:59 +0530

    browse events progress commit

--- src/app/components/common/browse-events/browse-events.component.ts
+++ src/app/components/common/browse-events/browse-events.component.ts
@@ -6,19 +6,22 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-browse-events',
   templateUrl: './browse-events.component.html',
-  styleUrls: ['./browse-events.component.css']
+  styleUrls: ['./browse-events.component.css'],
 })
 export class BrowseEventsComponent implements OnInit {
   allEvents: IEvent[];
+  categories: any[] = ['Seminar', 'Conference', 'Workshop', 'Reunion', 'Party'];
 
-  constructor(private commonService: CommonService, private userService: UserService) { }
+  constructor(
+    private commonService: CommonService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.fetchAllEvents();
   }
 
-  fetchAllEvents(){
+  fetchAllEvents() {
     this.allEvents = this.commonService.fetchAllEvents();
   }
-
 }
