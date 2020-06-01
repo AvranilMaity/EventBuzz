111d07e799066eb1391b49bf59cbc51254587400
Kingshuk <saha.kingshuk@yahoo.com>
2020-06-02 00:53:32 +0530

    adding enums

---
+++ src/app/utilities/constants.ts
@@ -0,0 +1,27 @@
+export enum EventCategory {
+    Seminar,
+    Conference,
+    Workshop,
+    Reunion,
+    Party,
+}
+
+export enum EventType {
+    Open,
+    Closed
+}
+
+export enum TicketType {
+    Regular,
+    VIP
+}
+
+export enum OrganizedType {
+    Private,
+    Corporate
+}
+
+export enum HeadCount {
+    Limited,
+    Unlimited
+}
