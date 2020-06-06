ee93b3abf209437e0de4c5f9010c675090cc4978
AvranilMaity <avranilmaity97@gmail.com>
2020-06-07 00:56:28 +0530

    ticket

--- src/app/components/common/confirmation/confirmation.component.ts
+++ src/app/components/common/confirmation/confirmation.component.ts
@@ -22,19 +22,23 @@ export class ConfirmationComponent implements OnInit {
     this.ticketType = this.ticketType.toUpperCase();
   }
 
-  downloadTicket(){
-    var data = document.getElementById('ticket');  //Id of the table
-    html2canvas(data).then(canvas => {  
-      // Few necessary setting options  
-      let imgWidth = 208;   
-      let pageHeight = 295;    
-      let imgHeight = canvas.height * imgWidth / canvas.width;  
-      let heightLeft = imgHeight;  
+  downloadTicket() {
+    var data = document.getElementById('ticket'); //Id of the table
+    html2canvas(data).then((canvas) => {
+      // Few necessary setting options
+      let imgWidth = 208;
+      let pageHeight = 295;
+      console.log(canvas.height);
 
-      const contentDataURL = canvas.toDataURL('image/png')  
-      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
-      let position = 0;  
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      let imgHeight = (canvas.height * imgWidth) / canvas.width;
+      console.log(imgHeight);
+
+      let heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png');
+      let pdf = new jspdf('p', 'mm'); // A4 size page of PDF
+      let position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
       pdf.save('ticket.pdf'); // Generated PDF
     });
   }
