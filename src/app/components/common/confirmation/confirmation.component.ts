777592fa52cb2275300186ba2a07d09aa1b3e2f5
Kingshuk <saha.kingshuk@yahoo.com>
2020-06-06 22:51:17 +0530

    download ticket functionality added

--- src/app/components/common/confirmation/confirmation.component.ts
+++ src/app/components/common/confirmation/confirmation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-confirmation',
@@ -18,5 +20,22 @@ export class ConfirmationComponent implements OnInit {
     this.eventLocation = this.eventLocation.toUpperCase();
     this.username = this.username.toUpperCase();
     this.ticketType = this.ticketType.toUpperCase();
+  }
+
+  downloadTicket(){
+    var data = document.getElementById('ticket');  //Id of the table
+    html2canvas(data).then(canvas => {  
+      // Few necessary setting options  
+      let imgWidth = 208;   
+      let pageHeight = 295;    
+      let imgHeight = canvas.height * imgWidth / canvas.width;  
+      let heightLeft = imgHeight;  
+
+      const contentDataURL = canvas.toDataURL('image/png')  
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      let position = 0;  
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.save('ticket.pdf'); // Generated PDF
+    });
   }
 }
