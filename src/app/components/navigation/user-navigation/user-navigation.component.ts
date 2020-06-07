bc2f397f3f5aee3597b75796fd62c8267288d156
AvranilMaity <avranilmaity97@gmail.com>
2020-06-07 20:29:22 +0530

    nav fix

--- src/app/components/navigation/user-navigation/user-navigation.component.ts
+++ src/app/components/navigation/user-navigation/user-navigation.component.ts
@@ -6,26 +6,26 @@ import { IAuthUser } from 'src/app/interfaces/auth-user';
 @Component({
   selector: 'app-user-navigation',
   templateUrl: './user-navigation.component.html',
-  styleUrls: ['./user-navigation.component.css']
+  styleUrls: ['./user-navigation.component.css'],
 })
 export class UserNavigationComponent implements OnInit {
-
-  constructor(private route: Router, private commonService: CommonService) { }
-  accountEnabled:boolean = false;
+  constructor(private route: Router, private commonService: CommonService) {}
+  accountEnabled: boolean = false;
   ngOnInit() {
-    var user:IAuthUser = JSON.parse(localStorage.getItem('user'));
-    if(user!= null && user.user.isEnabled==false)
-    {
+    var user: IAuthUser = JSON.parse(localStorage.getItem('user'));
+    if (user != null && user.user.isEnabled == false) {
       this.accountEnabled = false;
+    } else {
+      this.accountEnabled = true;
     }
   }
-  onSignOut(){
+  onSignOut() {
     console.log('Click on sign in button');
     localStorage.removeItem('user');
     this.route.navigate(['/landing']);
   }
 
-  createEvent(){
+  createEvent() {
     console.log('Click on create event button');
     this.route.navigate(['/eventaddedit']);
   }
