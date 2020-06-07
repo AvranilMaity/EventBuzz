8487857dbbccb175ed6b5f52251a04cbb10724a8
AvranilMaity <avranilmaity97@gmail.com>
2020-06-07 20:00:04 +0530

    responsive fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    setTimeout(function () {
+      let viewheight = $(window).height();
+      let viewwidth = $(window).width();
+      let viewport = document.querySelector('meta[name=viewport]');
+      viewport.setAttribute(
+        'content',
+        'height=' +
+          viewheight +
+          'px, width=' +
+          viewwidth +
+          'px, initial-scale=1.0'
+      );
+    }, 300);
+  }
   title = 'EventBuzz';
 }
